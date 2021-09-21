14a2dd0ff88591882b30b5b558a10a062b0d6040
Initial Commit <n01382533@humbermail.ca>
2021-09-20 22:28:22 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Smart Prosthetic Arm
+## Kevin Santizo n01382533
+## Narendre Ramdhan (Alex) n01240746
+## Samad Agha n01364908
+## Henry To n01365792
+###https://github.com/KevinSantizo2533/SmartProstheticArm
+###This application is to demonstrate the functionality of the smart prosthetic arm's app
---
+++ app/src/main/java/ca/kash/it/smartprostheticarm/MainActivity.java
@@ -0,0 +1,60 @@
+/*
+    Kevin Santizo n01382533
+    (Alex) Narendre Ramdhan n01240746
+    Samad Agha n01364908
+    Henry To n01365792
+*/
+package ca.kash.it.smartprostheticarm;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+    @Override
+
+    public void onBackPressed(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(R.string.Dialog_Exit);
+        builder.setCancelable(true);
+        builder.setNegativeButton(R.string.onBack_No, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setPositiveButton(R.string.OnBack_Close, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+}
