9a93db93ccd01b6d8353ec938e6313a98d93f8b8
Initial Commit <n01382533@humbermail.ca>
2021-10-06 15:15:42 -0400

    1st commit of deliverable 2. Setup the settings screen/fragment and have added one feature which is adjusting the screen orientation. Still need to add 3 more features to the settings screen.

---
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/settings/SettingsFragment.java
@@ -0,0 +1,15 @@
+package ca.kash.it.smartprostheticarm.ui.settings;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import ca.kash.it.smartprostheticarm.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
