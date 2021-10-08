48e2c8f87f89d230367fac90193c0fa0d088a76a
Samad <n01364908@humbermail.ca>
2021-10-08 00:09:20 -0400

    Created button redirects and fixes - Clicking register takes user to register activity - Clicking login takes user to main activity - Changed primary colour to "companyblue"

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/LoginActivity.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/LoginActivity.java
@@ -2,13 +2,42 @@ package ca.kash.it.smartprostheticarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
+    }
+
+
 }
