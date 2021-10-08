934ad0be74d9d21d24a8b1e5be22fc8d2d1c8ac9
Initial Commit <n01382533@humbermail.ca>
2021-10-08 15:40:11 -0400

    Fully completed the configuration and the sensor(object,temp) screens.

---
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/TemperatureActivity.java
@@ -0,0 +1,22 @@
+/*
+    Kevin Santizo n01382533 Section B
+    (Alex) Narendre Ramdhan n01240746 Section B
+    Samad Agha n01364908 Section B
+    Henry To n01365792 Section B
+*/
+package ca.kash.it.smartprostheticarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class TemperatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+    }
+
+
+}
