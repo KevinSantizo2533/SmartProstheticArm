b0c9f041dc2ff5b6971ba2d344e71424cf476829
Samad <n01364908@humbermail.ca>
2021-11-14 02:44:10 -0500

    Completed Login with firebase credential

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/LoginActivity.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/LoginActivity.java
@@ -13,6 +13,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,7 +38,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private TextView register1;
     private TextView forgotpass;
-    private EditText editTextEmail, editTextPassword;
+    private EditText loginEmail, loginPass;
     private Button loginBtn;
     private FirebaseAuth mAuth;
     private ProgressBar progressbar;
@@ -63,15 +64,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        mAuth = FirebaseAuth.getInstance();
         register1 = (TextView) findViewById(R.id.register);
         register1.setOnClickListener(this);
         forgotpass = (TextView) findViewById(R.id.forgotpass);
         forgotpass.setOnClickListener(this);
         loginBtn = (Button)findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(this);
-        editTextEmail = (EditText)  findViewById(R.id.email);
-        editTextPassword = (EditText) findViewById(R.id.password);
+        loginEmail = (EditText)  findViewById(R.id.loginEmail);
+        loginPass = (EditText) findViewById(R.id.loginPass);
         progressbar = (ProgressBar) findViewById(R.id.progressbar);
 
     }
@@ -137,51 +138,51 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(new Intent(this, ForgotPasswordActivity.class));
                 break;
             case R.id.loginBtn:
-                startActivity(new Intent(this, MainActivity.class));
+                loginUser();
                 break;
         }
 
     }
 
-//    private void loginUser() {
-//        String password = editTextPassword.getText().toString().trim();
-//        String email = editTextEmail.getText().toString().trim();
-//
-//        if(email.isEmpty()){
-//            editTextEmail.setError("Email is required");
-//            editTextEmail.requestFocus();
-//            return;
-//        }
-//        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-//            editTextEmail.setError("Email invalid");
-//            editTextEmail.requestFocus();
-//            return;
-//        }
-//
-//        if(password.isEmpty()){
-//            editTextPassword.setError("Password is required");
-//            editTextPassword.requestFocus();
-//            return;
-//        }
-//
-//        if (password.length()<6){
-//            editTextPassword.setError(getString(R.string.passMin));
-//            editTextPassword.requestFocus();
-//            return;
-//        }
-//
-//        progressbar.setVisibility(View.VISIBLE);
-//        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//                if(task.isSuccessful()){
-//                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-//                }else{
-//                    Toast.makeText(LoginActivity.this,"Failed to login",Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
-//    }
+    private void loginUser() {
+        String logPassword = loginPass.getText().toString().trim();
+        String logEmail = loginEmail.getText().toString().trim();
+
+        if(logEmail.isEmpty()){
+            loginEmail.setError("Email is required");
+            loginEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(logEmail).matches()){
+            loginEmail.setError("Email invalid");
+            loginEmail.requestFocus();
+            return;
+        }
+
+        if(logPassword.isEmpty()){
+            loginPass.setError("Password is required");
+            loginPass.requestFocus();
+            return;
+        }
+
+        if (logPassword.length()<6){
+            loginPass.setError(getString(R.string.passMin));
+            loginPass.requestFocus();
+            return;
+        }
+
+        progressbar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(logEmail, logPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }else{
+                    Toast.makeText(LoginActivity.this,"Failed to login",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
     @Override
     public void onBackPressed(){
