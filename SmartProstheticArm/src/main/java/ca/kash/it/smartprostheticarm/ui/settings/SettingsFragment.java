a3b7ea922308ef2b9f56685ed8f8b73aeecd3ed1
henry <henryto873@gmail.com>
2021-11-13 21:45:53 -0500

    Created customer review screen Added feedback button on settings screen

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/settings/SettingsFragment.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package ca.kash.it.smartprostheticarm.ui.settings;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -10,12 +11,24 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
 import ca.kash.it.smartprostheticarm.R;
+import ca.kash.it.smartprostheticarm.ReviewActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        Preference p = (Preference) findPreference("review");
+        p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+
+                Intent intent = new Intent(getActivity(), ReviewActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+
     }
 
     private void LoadPreferences() {
