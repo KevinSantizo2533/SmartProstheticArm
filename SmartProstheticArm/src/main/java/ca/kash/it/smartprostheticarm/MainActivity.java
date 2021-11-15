53ae40d662e644ed9e3bd5eeb84628542cf6fb79
henry <henryto873@gmail.com>
2021-11-14 19:07:27 -0500

    Added functionality for remember me checkbox

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/MainActivity.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/MainActivity.java
@@ -111,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
                 builder2.setPositiveButton(R.string.OnBack_Close, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
                         FirebaseAuth.getInstance().signOut();
                         startActivity(new Intent(MainActivity.this, LoginActivity.class));
                     }
