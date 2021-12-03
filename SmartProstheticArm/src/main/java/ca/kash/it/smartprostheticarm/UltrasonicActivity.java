65704f73b36f9f59407f964318d84ef61ebc033d
Initial Commit <n01382533@humbermail.ca>
2021-12-02 21:42:34 -0500

    Setup UltrasonicActivity for firebase For DB: Buttons TextView

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/UltrasonicActivity.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/UltrasonicActivity.java
@@ -6,17 +6,29 @@
 */
 package ca.kash.it.smartprostheticarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.BreakIterator;
 
 
 public class UltrasonicActivity extends AppCompatActivity {
+    TextView UltrasonicReading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultrasonic);
+        UltrasonicReading = findViewById(R.id.getUltrasonicReading);
+
     }
-
-
 }
