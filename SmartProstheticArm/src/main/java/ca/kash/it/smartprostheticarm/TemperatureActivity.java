5cce4079d5807a88cda90bba06f717d0d82ba3ba
henry <henryto873@gmail.com>
2021-12-02 23:27:35 -0500

    Implemented retrieving temperature sensor data from firebase

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/TemperatureActivity.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/TemperatureActivity.java
@@ -6,16 +6,42 @@
 */
 package ca.kash.it.smartprostheticarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class TemperatureActivity extends AppCompatActivity {
+    TextView TemperatureReading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+
+        TemperatureReading = findViewById(R.id.temperaturereading);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        Query lastQuery = databaseReference.child("sensor").child("temperature").orderByKey().limitToLast(1);
+        lastQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    String reading = data.child("reading").getValue().toString();
+                    TemperatureReading.setText(reading);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 
 
