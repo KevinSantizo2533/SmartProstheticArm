7e158b34d63f42c8f57b27ed7ae6d697537248e5
Initial Commit <n01382533@humbermail.ca>
2021-12-04 13:54:52 -0500

    Created test class for main activity (Junit 4)

---
+++ SmartProstheticArm/src/test/java/ca/kash/it/smartprostheticarm/MainActivityTest.java
@@ -0,0 +1,53 @@
+package ca.kash.it.smartprostheticarm;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+    private MainActivity mActivity;
+
+    @Before
+    public void setup() throws Exception {
+        mActivity = new MainActivity();
+    }
+
+    @Test
+    public  void testCase1(){
+        mActivity = new MainActivity();
+
+
+    }
+
+    @Test
+    public void testCase2(){
+        mActivity = new MainActivity();
+
+
+    }
+
+    @Test
+    public void testCase3(){
+        mActivity = new MainActivity();
+
+
+    }
+
+    @Test
+    public void testCase4(){
+        mActivity = new MainActivity();
+
+
+    }
+
+    @Test
+    public void testCase5(){
+        mActivity = new MainActivity();
+
+
+    }
+
+
+}
