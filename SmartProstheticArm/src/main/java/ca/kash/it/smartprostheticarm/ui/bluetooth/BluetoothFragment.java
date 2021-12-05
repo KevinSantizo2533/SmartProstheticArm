2d4d8a2884bc267764ba64fc17681a85bae0a5d2
Alex <n01240746@humbermail.ca>
2021-12-05 03:05:59 -0500

    Fixed crash when accessing sensor screen

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/bluetooth/BluetoothFragment.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/bluetooth/BluetoothFragment.java
@@ -37,14 +37,12 @@ import ca.kash.it.smartprostheticarm.R;
 
 public class BluetoothFragment extends Fragment {
 
-    private static final int PERMISSIONS_REQUEST_BLUETOOTH = 100;
-    Button btbutton;
     TextView Servo;
 
-    private BluetoothViewModel bluetoothViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_bluetooth, container, false);
+
         Servo = root.findViewById(R.id.servoreading);
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         Query lastQuery = databaseReference.child(getString(R.string.sensorchild)).child(getString(R.string.servo)).orderByKey().limitToLast(1);
@@ -56,46 +54,11 @@ public class BluetoothFragment extends Fragment {
                     Servo.setText(getString(R.string.direction) + reading);
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
-
-
-        btbutton = root.findViewById(R.id.bluetoothbtn);
-        btbutton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_BLUETOOTH
-                    );
-                } else {
-                    Snackbar snackBar = Snackbar.make(getActivity().findViewById(android.R.id.content), R.string.BTGranted, Snackbar.LENGTH_LONG);
-                    snackBar.show();
-                }
-            }
-        });
         return root;
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_BLUETOOTH: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Snackbar snackBar = Snackbar.make(getActivity().findViewById(android.R.id.content), R.string.BTGranted, Snackbar.LENGTH_LONG);
-                    snackBar.show();
-                } else {
-                    Snackbar snackBar = Snackbar.make(getActivity().findViewById(android.R.id.content), R.string.BTDenied, Snackbar.LENGTH_LONG);
-                    snackBar.show();
-                }
-                return;
-            }
-        }
-    }
-
-
 }
