f7b8d0ee5f51e66429d5e28e5d7de6e26faafe67
henry <henryto873@gmail.com>
2021-12-07 21:06:01 -0500

    Arm controller fragment fix

--- SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/armcontroller/ArmControllerFragment.java
+++ SmartProstheticArm/src/main/java/ca/kash/it/smartprostheticarm/ui/armcontroller/ArmControllerFragment.java
@@ -113,12 +113,12 @@ public class ArmControllerFragment extends Fragment {
 
         });
         Servo = root.findViewById(R.id.servoreading);
-        Query lastQuery = databaseReference.child(getString(R.string.sensorchild)).child(getString(R.string.servo)).orderByKey().limitToLast(1);
+        Query lastQuery = databaseReference.child("sensor").child("servo").orderByKey().limitToLast(1);
         lastQuery.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot data : dataSnapshot.getChildren()) {
-                    String reading = data.child(getString(R.string.readingchild)).getValue().toString();
+                    String reading = data.child("direction").getValue().toString();
                     Servo.setText(getString(R.string.direction) + reading);
                 }
             }
